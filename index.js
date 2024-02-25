74814b97ce291c24f5f315f3ceb11cc3436cb5a2
OHshajim <ajshajimmax@gmail.com>
2024-02-25 22:38:41 +0600

    computer chose

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const options = document.querySelectorAll('.pic')
 
+// computer chose
+const computerChose = () => {
+    const option = ["Stone","Paper","Scissors"]
+    const index = Math.floor(Math.random()*3);
+    return option[index]
+}
+
+const gamePlay =(Id) =>{
+    console.log("user chose :", Id);
+    const computer = computerChose()
+    console.log("computer chose :", computer);
+}
+
+
 options.forEach((option) => {
     option.addEventListener("click", function () {
-        console.log(option);
+        // console.log(option);
         option.classList.add('border-orange-800')
         const choseID = option.getAttribute("id")
-        console.log(choseID);
+
+        gamePlay(choseID)
     })
 }); 
